756cc6968e54ea0d24440854191679fcc8d8d4e7
Christopher Karl Snowflake <119427148+chriskarlchan@users.noreply.github.com>
2022-11-29 15:32:58 +0100

    Update streamlit_app.py
    Added in breakfase

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner is Soup')
+
+strealit.text('Breakfast Menu')
+strealit.text('Some fresh bread')
+strealit.text('Some cheese')
+strealit.text('Crispy bacon')
+
+
