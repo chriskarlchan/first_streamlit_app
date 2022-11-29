6bb98518c3c214f94c19d05e8ff52a7d5827f29c
Christopher Karl Snowflake <119427148+chriskarlchan@users.noreply.github.com>
2022-11-29 15:33:33 +0100

    Update streamlit_app.py
    Corrected the typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner is Soup')
 
-strealit.text('Breakfast Menu')
-strealit.text('Some fresh bread')
-strealit.text('Some cheese')
-strealit.text('Crispy bacon')
+streamlit.text('Breakfast Menu')
+streamlit.text('Some fresh bread')
+streamlit.text('Some cheese')
+streamlit.text('Crispy bacon')
 
 
