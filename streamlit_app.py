d47c66d571bcdcccf614a837a83a092c631aa607
Christopher Karl Snowflake <119427148+chriskarlchan@users.noreply.github.com>
2022-12-14 17:29:06 +0100

    Added in try catch and comments code to stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector #Reads requirements from a file
+#Needed for control of flow
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner is Soup')
 
@@ -30,18 +32,20 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruitviceapi response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice
+    streamlit.error("please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# Normalize the json code
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# output to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
+except URLError as e:
+    streamlit.error()
+    
 #don't run anything past here while we troubleshoot
+streamlit.stop
 
 #Snowflake code
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
